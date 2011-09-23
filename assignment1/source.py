c181adbf8dd79a6ea1ea306851fee39c78ad4b09
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-09-23 13:17:18 -0400

    initial commit

---
+++ assignment1/source.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+#import 
+
+def main():
+
+Student = { "John" : { "join_date" : "05/03/2011", "Percent" : 80.055 }, "Don" : { "join_date" : "05/10/2011", "Percent" : 75.06777 }, "Smith" : { "join_date" : "04/04/2011", "Percent" : 85.8005 }}
+  
+  
+  
+if __name__ == '__main__':
+  main()
+
