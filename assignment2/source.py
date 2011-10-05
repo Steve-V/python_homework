aab15162fac5bbb1ce4fcea823cd704b62644fc6
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-10-05 01:04:14 -0400

    added assignment2 source

---
+++ assignment2/source.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+def main():
+  calc()
+  guess()
+  stack()
+  
+
+def calc():
+  '''1) Prompt the user to input two numbers and one operator among (+, -, *, /). Depending on the operator, perform the calculation and print the result.'''
+  pass
+
+
+def guess():
+  '''2) Write a program to generate a random number(Rnumber) between 1 and 10. (Use randint method from random module). Ask for user to guess a number(Gnumber)
+  between 1 and 10. Loop till the user guesses the right number. If Gnumber is less than Rnumber print the message “Your guess is too low”. If Gnumber is greater than Rnumber print the message “Your guess is too high”. Print the total number of guesses. '''
+  pass
+
+
+def stack():
+  '''3) Implement stack(First In First Out) using list with below operations.
+  a) Ask the user choice – push, pop OR quit
+  b) If push, add the value entered by the user and display the new list.
+  c) If pop, display the element popped and the new list.
+  d) Push/Pop operations to be carried out till the user quits
+  '''
+  pass
+
+
+
+if __name__ == '__main__':
+  main()
+
