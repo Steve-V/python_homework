41be18966aab07b8e27436c3b526c2bdf7cf6483
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-10-06 16:31:21 -0400

    finished stack

--- assignment2/source.py
+++ assignment2/source.py
@@ -2,7 +2,7 @@
 
 def main():
   #calc()
-  guess()
+  #guess()
   stack()
   
 
@@ -53,19 +53,26 @@ def guess():
   import random
   correct = random.randint(1,10)
   guess = 12
+  attempts = 0
   
   while guess != correct:
+    attempts += 1
     try:
       guess = int( input( "Enter a guess: " ) )
     except ValueError:
       print("Not even close.")
       continue
     
+    #outside the bounds of the game
+    if guess not in range(0,10):
+      print("Try guessing numbers between 1 and 10")
+      continue
+    
     #too high / low
     if guess > correct: print("Your guess is too high!")
     if guess < correct: print("Your guess is too low!")
   
-  print("Correct!")
+  print("Correct! Wow! First guess!") if attempts == 1 else print("Correct! You used {} guesses!".format(attempts) )
 
 
 def stack():
@@ -73,12 +80,37 @@ def stack():
   a) Ask the user choice – push, pop OR quit
   b) If push, add the value entered by the user and display the new list.
   c) If pop, display the element popped and the new list.
-  d) Push/Pop operations to be carried out till the user quits
-  '''
+  d) Push/Pop operations to be carried out till the user quits'''
   
+  #set up the data storage
+  storage = []
   
+  #set up the loop
+  while True:
+    #get user command
+    command = str( input("(A)ppend / (R)emove / (Q)uit: ") )
+    
+    if command.lower() == "a":
+      #then append
+      storage.append( input("Enter value: " ) )
+      showStorage(storage)
+    
+    elif command.lower() == "r":
+      #then pop
+      print("Removed item: {}".format( storage.pop(0) ) ) if storage else print("Can't pop from empty stack")
+      showStorage(storage)
+    
+    else:
+      #assume quit
+      print("Quitting!")
+      return()
 
-
+def showStorage(storage):
+  '''Output the storage list'''
+  if not storage:
+    print("Stack is empty")
+  else:
+    print(storage)
 
 if __name__ == '__main__':
   main()
