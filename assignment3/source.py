3d6bca9084b227b5335dbe970a932f57f8f46c5e
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-10-13 18:57:03 -0400

    created source file

---
+++ assignment3/source.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+#import 
+
+def main():
+  
+  reverseString()
+  swapVariables()
+  isPrime()
+  fibonacci()
+
+
+def reverseString():
+  '''Write a function to reverse a string provided by the user. Display the string before and after reversing. The default string value is “Greeting”.'''
+  pass
+
+
+def swapVariables():
+  '''Write a function to swap two variables “a and b”. Variables can be int, string, list or dictionaries.'''
+  pass
+
+
+def isPrime():
+  '''Return 1 if an argument is a prime number and 0 otherwise'''
+  pass
+
+
+def fibonacci():
+  '''Write a recursive program to generate Fibonacci series for n numbers'''
+  pass
+
+  
+if __name__ == '__main__':
+  main()
