c003f0ff372abb1ae727b6880836534b5a99e969
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-11-08 19:02:10 -0500

    created gui, added package stuff

---
+++ inclass.gui/gui.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import wx
+
+def ShowWindow():
+  app = wx.App()
+  win = wx.Frame(None)
+  win.Show()
+  
+
+def main():
+  
+  
+  
+# Remember to unindent this line!
+if __name__ == '__main__':
+  main()
---
+++ inclass.package/mypack/mymodule.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+#import 
+
+'''
+  Assignment:
+    Create package mypack
+    create __init.py__ within mypack
+    creat module mymodule.py within mypack
+    define functions add, mul (add, multiply) in mymodule.py
+      add and mul each take 2 paramaters
+    be able to import mypack.mymodule as test
+    test.add() must work
+    test.mul() must work
+'''
+
+def add(var1, var2): return var1+var2
+def mul(var1, var2): return var1*var2
+
+
+def main():
+  pass
+  
+  
+# Remember to unindent this line!
+if __name__ == '__main__':
+  main()
