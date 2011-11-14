c90a5ef95dd53d7c759e94d224d3d795f14d175b
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-11-14 18:09:39 -0500

    created assignment 4 source

---
+++ assignment4/source.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import unittest
+
+class Rectangle:
+    # Setup
+    def __init__(self,length=1,width=1):
+        self.__length = length
+        self.__width = width
+    
+    # Get and set
+    def setLength(self, new):
+        self.__length = new
+    def setWidth(self, new):
+        self.__width = new
+    def getLength(self):
+        return self.__length
+    def getWidth(self):
+        return self.__width
+    
+    # Calculations
+    def perimeter(self):
+        return (2*self.__length) + (2*self.__width)
+    def area(self):
+        return self.__length * self.__width
+    
+
+class TestRectangle(unittest.TestCase):
+    # Build a rectangle that can be used for the tests
+    def setUp(self):
+        self.r = Rectangle(2,5)
+    
+    def test_checkPerimeter(self):
+        '''Checking perimeter of rectangle'''
+        self.assertEqual(self.r.perimeter(), 14)
+    
+    def test_checkArea(self):
+        '''Checking area of rectangle'''
+        self.assertEqual(self.r.area(), 10)
+    
+    def 
+    
+def main():
+    unittest.main()
+    
+    
+    
+# Remember to unindent this line!
+if __name__ == '__main__':
+    main()
