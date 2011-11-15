994aed6a98035c4c1cafbd6475abe1f33fe861e2
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-11-14 23:10:06 -0500

    renamed rectangle, added shelve

--- assignment4/rectangle.py
+++ assignment4/rectangle.py
@@ -5,20 +5,32 @@ import unittest
 class outOfRangeException(Exception):
     pass
 
-class NANException(Exception):
+class NanException(Exception):
     pass
 
 class Rectangle:
     # Setup
     def __init__(self,length=1.0,width=1.0):
+        # We use the setters so we get error checking for free
         self.setLength(length)
         self.setWidth(width)
     
+    def checkValue(self, new):
+        # This way we don't have to have the same code in multiple places
+        try:
+            float(new)
+        except ValueError:
+            raise NanException
+        if new <= 0 or new >= 20:
+            raise outOfRangeException
+        else:
+            return new
+    
     # Get and set
     def setLength(self, new):
-        self.__length = new
+        self.__length = self.checkValue(new)
     def setWidth(self, new):
-        self.__width = new
+        self.__width = self.checkValue(new)
     def getLength(self):
         return self.__length
     def getWidth(self):
@@ -44,9 +56,20 @@ class TestRectangle(unittest.TestCase):
         '''Checking area of rectangle'''
         self.assertEqual(self.r.area(), 10)
     
+    def test_checkOutOfBoundsValue_high(self):
+        '''Checking out of bounds value trap - high'''
+        self.assertRaises(outOfRangeException, self.r.setLength, 30)
+    
+    def test_checkOutOfBoundsValue_low(self):
+        '''Checking out of bounds value trap - low'''
+        self.assertRaises(outOfRangeException, self.r.setLength, -3)
+    
+    def test_checkNanException(self):
+        '''Checking if an exception is thrown when input is not a number'''
+        self.assertRaises(NanException, self.r.setLength, "farts")
     
 def main():
-    unittest.main(verbosity=2)
+    unittest.main()
     
     
     
---
+++ assignment4/shelve.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+#import 
+
+def main():
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
