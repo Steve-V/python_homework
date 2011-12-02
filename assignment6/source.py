d6cbac8647c8d9a4589a01a37b8615145926f989
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-12-01 19:21:38 -0500

    added assignment definitons

--- assignment6/source.py
+++ assignment6/source.py
@@ -2,8 +2,46 @@
 
 #import 
 
-def main():
+def hardware():
+     '''You are the owner of a hardware store and have to keep an inventory that can tell you what
+     tools you have, how many of each tools you have and the cost of each tool.
+     Create a database called Hardware_Store and a table called inventory with primary key
+     Tool_name with the following data.
+     Write a program with SQL commands to
+     a) Insert details of a new tool
+     b) Retrieve data of a tool and display
+     c) Provide option to update tool information(Quality and cost)
+     '''
+     
+     
+     
+    pass
+
+def barchart():
+    '''
+     Using matplotlib for Python, draw a barchart for the following information
+        Month
+            April
+            May
+            June
+        Number of Visitors(Adult)
+            300
+            500
+            700
+        Number of Visitors(Children)
+            200
+            600
+            600
+    '''
     
+    pass
+
+
+
+
+def main():
+    hardware()
+    barchart()
     
     
 # Remember to unindent this line!
