1a3fec1d07f0604df3445b24cd818beccebe51ac
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-12-08 10:53:00 -0500

    created barchart

--- assignment6/barchart.py
+++ assignment6/barchart.py
@@ -3,41 +3,25 @@
 #import 
 
 def main():
-    '''
-     Using matplotlib for Python, draw a barchart for the following information
-        Month
-            April
-            May
-            June
-        Number of Visitors(Adult)
-            300
-            500
-            700
-        Number of Visitors(Children)
-            200
-            600
-            600
-    '''
+    '''Draw a barchart with matplotlib'''
     import numpy as np
     import matplotlib.pyplot as plt
 
 
-    N = 5
-    menMeans   = (20, 35, 30, 35, 27)
-    womenMeans = (25, 32, 34, 20, 25)
-    menStd     = (2, 3, 4, 1, 2)
-    womenStd   = (3, 5, 2, 3, 3)
-    ind = np.arange(N)    # the x locations for the groups
-    width = 0.35       # the width of the bars: can also be len(x) sequence
+    N = 3
+    adults   = (300, 500, 700)
+    kids = (200,600,600)
+    ind = np.arange(N)
+    width = 0.35
 
-    p1 = plt.bar(ind, menMeans,   width, color='r', yerr=womenStd)
-    p2 = plt.bar(ind, womenMeans, width, color='y', bottom=menMeans, yerr=menStd)
+    p1 = plt.bar(ind, adults,   width, color='r')
+    p2 = plt.bar(ind+width, kids, width, color='b')
 
-    plt.ylabel('Scores')
-    plt.title('Scores by group and gender')
-    plt.xticks(ind+width/2., ('G1', 'G2', 'G3', 'G4', 'G5') )
-    plt.yticks(np.arange(0,81,10))
-    plt.legend( (p1[0], p2[0]), ('Men', 'Women') )
+    plt.ylabel('Visitors')
+    plt.title('Visitors by month')
+    plt.xticks(ind+width/2., ('April', 'May', 'June') )
+    plt.yticks(np.arange(0,1000,100))
+    plt.legend( (p1[0], p2[0]), ('Adults', 'Kids') )
 
     plt.show()
 
